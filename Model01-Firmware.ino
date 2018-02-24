ca25755e0cbca473e6b2c750b2f4d87a4d8e1d84
Keith Simmons <keith@the-simmons.net>
2018-02-24 15:39:10 -0800

    more keymap changes

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -11,28 +11,45 @@
 #include "LED-Off.h"
 
 enum { LEFT_BRACKET, RIGHT_BRACKET, ESCAPE };
-enum { QWERTY, STENO, FUNCTION};
+enum { QWERTY, GAMING, STENO, FUNCTION};
 
 // *INDENT-OFF*
 
 const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [QWERTY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, ___,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, TD(LEFT_BRACKET),
-   Key_Escape,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   ___,          Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Tab,
+  (XXX,           Key_1, Key_2, Key_3, Key_4, Key_5, XXX,
+   Key_Backtick,  Key_Q, Key_W, Key_E, Key_R, Key_T, TD(LEFT_BRACKET),
+   Key_Escape,    Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_LeftShift, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Tab,
    
    Key_LeftAlt, Key_Backspace, Key_LeftControl, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   ___,               Key_6, Key_7, Key_8,     Key_9,         Key_0,         ___,
+   XXX,               Key_6, Key_7, Key_8,     Key_9,         Key_0,         XXX,
    TD(RIGHT_BRACKET), Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                       Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   ___,               Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   XXX,               Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
    
    Key_RightShift, Key_Enter, Key_Spacebar, Key_LeftGui,
    ShiftToLayer(FUNCTION)),
+
+  [GAMING] = KEYMAP_STACKED
+  (___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   
+   Key_Space, Key_Backspace, ___, ___,
+   ___,
+
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+  
+   ___, ___, ___, ___,
+   ___),
 
   [STENO] = KEYMAP_STACKED
   (XXX,    XXX,   XXX,   XXX,   XXX,   XXX,   S(N6),
@@ -40,33 +57,33 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    S(FN),  S(S1), S(TL), S(PL), S(HL), S(ST1),
    S(PWR), S(S2), S(KL), S(WL), S(RL), S(ST2), S(ST2),
 
-   S(RE1), XXX, S(A), S(O),
-   ___,
+   S(RE1), S(A), S(O), XXX,
+   ShiftToLayer(FUNCTION),
 
    S(N7),  XXX,    XXX,   XXX,   XXX,   XXX,   XXX,
    S(ST3), S(N8),  S(N9), S(NA), S(NB), S(NC), XXX,
    S(ST3), S(FR),  S(PR), S(LR), S(TR), S(DR),
    S(ST4), S(ST4), S(RR), S(BR), S(GR), S(SR), S(ZR),
 
-   S(E), S(U), XXX, S(RE2),
-   ___),
-
+   XXX, S(E), S(U), S(RE2),
+   ShiftToLayer(FUNCTION)),
+   
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  ___,  ___,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
+  (XXX, Key_F1,     Key_F2,      Key_F3,            Key_F4,        Key_F5, XXX,
+   XXX, XXX,        Key_mouseUp, Key_mouseScrollUp, Key_mouseBtnR, XXX,    XXX,
+   XXX, Key_mouseL, Key_mouseDn, Key_mouseR,        Key_mouseBtnL, XXX,
+   XXX, XXX,        XXX,         Key_mouseScrollDn, Key_mouseBtnM, XXX,    LockLayer(GAMING),
    
-   ___, Key_Delete, ___, ___,
-   ___,
+   Key_LeftAlt, Key_Delete, Key_LeftControl, Key_LeftShift,
+   ShiftToLayer(FUNCTION),
 
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   LockLayer(STENO),          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
+   XXX,               Key_F6,               Key_F7,               Key_F8,             Key_F9,                Key_F10,       Key_F11,
+   LockLayer(STENO),  LCTRL(Key_LeftArrow), LCTRL(Key_DownArrow), LCTRL(Key_UpArrow), LCTRL(Key_RightArrow), XXX,           Key_F12,
+                      Key_LeftArrow,        Key_DownArrow,        Key_UpArrow,        Key_RightArrow,        XXX,           XXX,
+   LockLayer(QUERTY), Key_Home,             Key_PageDown,         Key_PageUp,         Key_End,               Key_Backslash, Key_Pipe,
    
-   ___, ___, ___, ___,
-   ___),
+   Key_RightShift, Key_Enter, Key_Tab, Key_RightGui,
+   ShiftToLayer(FUNCTION)),
 
 };
 
